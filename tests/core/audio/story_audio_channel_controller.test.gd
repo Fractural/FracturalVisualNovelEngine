883f085507aec68b10e82f86f35c56db2beaa8f3
Atlinx <atlinuxtm@gmail.com>
2022-08-12 01:54:55 -0400

    Fix unit tests

--- tests/core/audio/story_audio_channel_controller.test.gd
+++ tests/core/audio/story_audio_channel_controller.test.gd
@@ -59,8 +59,7 @@ func test_playing_audio_for_unskippable_controller():
 	asserts.is_equal(controller.get_current_sound(), SOUND_SAMPLE, "Then, @time = 1/2 of sample's legnth, the audio player is playing the correct sample.")
 
 	yield(until_signal(controller, "finished_playing", SOUND_SAMPLE.get_length() + 3), YIELD)
-	yield(until_timeout(1), YIELD)
-	
+
 	asserts.is_false(_is_audio_playing(), "Then, @time > sample's length, the audio player stops playing.")
 	asserts.is_null(controller.get_current_sound(), "Then, @time > sample's length, the audio player's stream is null.")
 	
@@ -185,4 +184,4 @@ func _is_audio_playing():
 	# WAT will attempt to run the repeats in parallel, which leads to
 	# all the threads playing sound on the same AudioServer (making it impossible
 	# to distinguish which server is what).
-	return AudioServer.get_bus_peak_volume_left_db(AudioServer.get_bus_index("Master"), 0) > -200
+	return AudioServer.get_bus_peak_volume_left_db(AudioServer.get_bus_index("Master"), 0) > -150
